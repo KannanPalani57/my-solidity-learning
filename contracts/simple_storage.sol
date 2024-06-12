8ac2667eb077c01e873065c12347fc4036cc67b5
Kannan <kannanpalani455@gmail.com>
2024-06-12 10:58:10 +0530

    add simple storage contract

---
+++ contracts/simple_storage.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract SimpleStorage {
+    uint256 public favoriteNumber; 
+
+    mapping(string => uint256) public nameOfFavNum;
+
+    struct Person {
+        uint256 favNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+
+
+
+    function store(uint256 _favoriteNumber, string memory _name) public {
+        favoriteNumber = _favoriteNumber;
+        
+        nameOfFavNum[_name] = _favoriteNumber;
+
+        listOfPeople.push( Person({
+            favNumber: _favoriteNumber, 
+            name: _name 
+        }));
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+
+    
+}
